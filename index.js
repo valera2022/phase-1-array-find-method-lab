d71b2d3c5a363710665098b51902c830e887778f
GeralmyUrena <geralmyuri11@gmail.com>
2022-11-28 18:55:08 -0500

    commit tree 32c98a04636fd96028eca640317ccd499305799d

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+
+// 
+
+
+
+//   function superbowlWin(array){ 
+//      return array.result === "W"
+//  }
+//  console.log(record.find(array).year)
+
+// function superbowlWin(array){
+//    return array.result === "W"  }
+   
+
+// array.find(superbowlWin)
+
+//  function superbowlWin(array){
+//    let found = array.find((item)=> { item.result ==="W"})
+//     if(found.result === "W"){
+//        return found
+//     }
+// }
+
+function superbowlWin(array) {
+   let found = array.find(item => item.result === "W")
+   if (found) 
+   {
+        return found.year
+  
+}
+}
